1610bbd22e57de34f8e7dbeb7f5145f528eebf6e
lyy289065406 <289065406@qq.com>
2018-10-25 16:45:53 +0800

    更新ruby练习

--- ruby/01-The Well-Grounded Rubyist/1-2/c2fi.rb
+++ ruby/01-The Well-Grounded Rubyist/1-2/c2fi.rb
@@ -6,6 +6,4 @@
 puts "Please enter a celsius value : "
 celsius = gets	# 摄氏度
 fahrenheit = celsius.to_i * 9 / 5 + 32	# 华氏度公式
-print "The result is : "	# print直接打印原生字符
-print fahrenheit
-puts " ."	# puts会在没有以换行符结束的字符串末尾添加换行符后再打印
+print "The result is : ", fahrenheit, " .\n"
--- ruby/01-The Well-Grounded Rubyist/1-4/c2fout.rb
+++ ruby/01-The Well-Grounded Rubyist/1-4/c2fout.rb
@@ -1,4 +1,4 @@
-# 练习1-2 ： 摄氏-华氏度转换（读取文件版）
+# 练习1-2 ： 摄氏-华氏度转换（读写文件版）
 
 puts "Read celsius value from file ..."
 value = File.read("temp.dat")
@@ -7,3 +7,8 @@ print "The celsius is : ", value, " .\n"
 celsius = value.to_i	# 摄氏度
 fahrenheit = celsius.to_i * 9 / 5 + 32	# 华氏度公式
 print "The result is : ", fahrenheit, " .\n"
+
+puts "Saving result to file ..."
+fh = File.new("temp.out", "w")
+fh.puts fahrenheit
+fh.close
